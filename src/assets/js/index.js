68b05de451e08e7dcd4611c9ba8ae79556f91721
mancjames <multicolouredsmurf@gmail.com>
2021-03-04 23:56:28 +0000

    updated javascript - added elements for cards and loop to allow all tags to come through from JSOn

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,37 +4,87 @@ function processData() {
   const data = request.response;
   const { photographers } = data;
   console.log(photographers);
+  console.log(photographers[0].tags);
 
   function showCards() {
     for (let i = 0; i < photographers.length; i++) {
+      // create elements
       const article = document.createElement('article');
-      article.className = 'card card__photographer';
-      cardContainer.appendChild(article);
-      const divPhotographerPortrait = document.createElement('div');
-      divPhotographerPortrait.className = 'card__photographer-portrait';
-      article.appendChild(divPhotographerPortrait);
       const photographerImage = document.createElement('img');
+      const divPhotographerTitle = document.createElement('div');
+      const divPhotographerPortrait = document.createElement('div');
+      const h2PhotographerTitle = document.createElement('h2');
+      const divPhotographerDescription = document.createElement('div');
+      const pPhotographerDescriptionLocation = document.createElement('p');
+      const pPhotographerDescriptionTagline = document.createElement('p');
+      const pPhotographerDescriptionPrice = document.createElement('p');
+      const divTag = document.createElement('div');
+      
+      
+      // Attaching classes to divs
+      divPhotographerPortrait.className = 'card__photographer-portrait';
+      article.className = 'card card__photographer';
+      divPhotographerTitle.className = 'card__photographer-title';
+      photographerImage.className = 'card__photographer-portrait-img';
+      h2PhotographerTitle.className = 'card__photographer-title-name';
+      divPhotographerDescription.className = 'card__photographer-description';
+      pPhotographerDescriptionLocation.className = 'card__photographer-description-location';
+      pPhotographerDescriptionTagline.className = 'card__photographer-description-tagline';
+      pPhotographerDescriptionPrice.className = 'card__photographer-description-price';
+      divTag.className = 'card__photographer-tags';
+      
+      
+      // Image Properties
       photographerImage.src = `./assets/img/Photographers ID Photos/${photographers[i].portrait}`;
       photographerImage.alt = `${photographers[i].name}`;
-      photographerImage.className = 'card__photographer-portrait-img';
-      divPhotographerPortrait.appendChild(photographerImage);
-      const divPhotographerTitle = document.createElement('div');
-      divPhotographerTitle.className = 'card__photographer-title';
-      article.appendChild(divPhotographerTitle);
-      const h2PhotographerTitle = document.createElement('h2');
-      h2PhotographerTitle.className = 'card__photographer-title-name';
+      // Anchor Properties
+
+      // adding text to elements
       h2PhotographerTitle.innerText = `${photographers[i].name}`;
+      pPhotographerDescriptionLocation.innerText = `${photographers[i].city}, ${photographers[i].country}`;
+      pPhotographerDescriptionTagline.innerText = `${photographers[i].tagline}`;
+      pPhotographerDescriptionPrice.innerText = `$${photographers[i].price}/day`;
+
+      for (let j = 0; j < photographers[i].tags.length; j++) {
+        const divTagAnchor = document.createElement('a');
+        const divTagSpan = document.createElement('span');
+        divTagAnchor.className = 'tag card__photographer-tags-item';
+        divTagSpan.className = 'sr-only';
+        divTagAnchor.innerText = `${photographers[i].tags[j]}`;
+        divTagSpan.innerText = `${photographers[i].tags[j]}`;
+        divTag.appendChild(divTagAnchor);
+        divTagAnchor.appendChild(divTagSpan);
+      };
+
+
+
+      // Appending elements to parent elements
+      cardContainer.appendChild(article);
+      article.append(divPhotographerPortrait, divPhotographerTitle, divPhotographerDescription);
+      divPhotographerPortrait.appendChild(photographerImage);
       divPhotographerTitle.appendChild(h2PhotographerTitle);
-      const divPhotographerDescription = document.createElement('div');
-      divPhotographerDescription.className = 'card__photographer-description';
-      article.appendChild(divPhotographerDescription);
-      const divTag = document.createElement('div');
-      divTag.className = 'card__photographer-tags';
-      divPhotographerDescription.appendChild(divTag);
+      divPhotographerDescription.append(pPhotographerDescriptionLocation,
+        pPhotographerDescriptionPrice,
+        pPhotographerDescriptionTagline,
+        divTag);
     }
   }
   showCards();
 }
+
+/*
+function createElement(elementName, element, className, parentName){
+    this.elementName = elementName;
+    this.element = element;
+    this.className = className;
+    this.parentName = parentName;
+    this.create = function() {
+        const this.elementName = document.createElement('`${this.element}`');
+        this.elementName.className = '`${this.className}`';
+        ${this.parentName}.appendChild(${this.elementName})
+    }
+}
+*/
 
 const requestURL = '../fisheyedata.json';
 const request = new XMLHttpRequest();
