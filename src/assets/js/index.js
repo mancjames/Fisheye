194c648d6806b9237026a0a5da821246d7a54ca8
mancjames <multicolouredsmurf@gmail.com>
2021-03-10 17:00:46 +0000

    added javascript for filter options

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,6 +7,7 @@ function processData() {
   console.log(photographers);
 
   function showCards() {
+    // class constructor for creating card elements
     class CreateCardElement {
       constructor(elementType, classProperties, parentName) {
         this.elementType = elementType;
@@ -17,7 +18,7 @@ function processData() {
         parentName.appendChild(this.element);
       }
     }
-
+    // extender to include additional properties needed for images
     class CreateCardElementImage extends CreateCardElement {
       constructor(elementType, classProperties, parentName, imgSrc, imgAlt) {
         super(elementType, classProperties, parentName);
@@ -27,18 +28,29 @@ function processData() {
         this.element.alt = imgAlt;
       }
     }
-
+    // extender to include additional elements needed to get JSON data
     class CreateCardElementDescription extends CreateCardElement {
-      constructor(elementType, classProperties, parentName, JSONtype) {
+      constructor(elementType, classProperties, parentName, JSONdata) {
         super(elementType, classProperties, parentName);
-        this.JSONtype = JSONtype;
-        this.element.innerText = this.JSONtype;
+        this.JSONdata = JSONdata;
+        this.element.innerText = JSONdata;
       }
     }
 
+    // loop created below so objects created dependent on length of photographer array in JSON
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < photographers.length; i++) {
-      const article = new CreateCardElement('article', 'card card__photographer', cardContainer);
+      // setting Data-tag's on cards to help with creating filter
+      // getting array from JSON
+      const tagArray = photographers[i].tags;
+      // Turning it into a string
+      const tagString = String(tagArray);
+      // Replacing commas to help seperate
+      const tagOptions = tagString.replace(/,/g, ' ');
+      // Adding to article element to help change display options for filter
+
+      // Created elements below by making each a new object from the class constructor created above
+      const article = new CreateCardElement('article', `card card__photographer ${tagOptions}`, cardContainer);
       const photographerPortrait = new CreateCardElement('div', 'card__photographer-portrait', article.element);
       const photographerImage = new CreateCardElementImage('img', 'card__photographer-portrait-img', photographerPortrait.element, `./assets/img/Photographers ID Photos/${photographers[i].portrait}`, `${photographers[i].name}`);
       const photographerTitle = new CreateCardElement('div', 'card__photographer-title', article.element);
@@ -51,17 +63,82 @@ function processData() {
 
       // Anchor Properties
 
-      // Loop for adding tags to end of card
+      // Loop to go through the tags array nested in the JSON photographer data
       for (let j = 0; j < photographers[i].tags.length; j++) {
-        const photographerTagsAnchor = new CreateCardElementDescription('a', 'tag card__photographer-tags-item', photographerTags.element, `${photographers[i].tags[j]}`);
+        const photographerTagsAnchor = new CreateCardElementDescription('li', 'tag card__photographer-tags-item', photographerTags.element, `${photographers[i].tags[j]}`);
         const photographerTagsSpan = new CreateCardElementDescription('span', 'sr-only', photographerTagsAnchor.element, `${photographers[i].tags[j]}`);
-        /*
-
-        divTagAnchor.setAttribute('data-tag', `${photographers[i].tags[j]}`);
-        */
       }
     }
+
+    const navList = document.querySelectorAll('.nav ul li');
+    // if statement options
+    const selectionArt = document.querySelectorAll('.art');
+    const selectionPortrait = document.querySelectorAll('.portrait');
+    const selectionFashion = document.querySelectorAll('.fashion');
+    const selectionArchitecture = document.querySelectorAll('.architecture');
+    const selectionTravel = document.querySelectorAll('.travel');
+    const selectionSport = document.querySelectorAll('.sport');
+    const selectionAnimals = document.querySelectorAll('.animals');
+    const selectionEvents = document.querySelectorAll('.events');
+
+    const cardSelections = document.querySelectorAll('.card__photographer');
+    // forEach loop to get each tag Option
+    navList.forEach((navListItem) => {
+      // adding Event Listener for selecting options
+      navListItem.addEventListener('click', () => {
+        navList.forEach((navListItem) => {
+          navListItem.classList.remove('active');
+        });
+
+        navListItem.classList.add('active');
+        var tagValue = navListItem.getAttribute('data-filter-tag');
+        console.log(tagValue);
+
+        cardSelections.forEach((cardSelection) => {
+          cardSelection.style.display = 'none';
+        });
+
+        if (tagValue == 'art') {
+          selectionArt.forEach((cardArt) => {
+            cardArt.style.display = 'block';
+          });
+        } else if (tagValue == 'portrait') {
+          selectionPortrait.forEach((cardPortrait) => {
+            cardPortrait.style.display = 'block';
+          });
+        } else if (tagValue == 'fashion') {
+          selectionFashion.forEach((cardFashion) => {
+            cardFashion.style.display = 'block';
+          });
+        } else if (tagValue == 'architecture') {
+          selectionArchitecture.forEach((cardArchitecture) => {
+            cardArchitecture.style.display = 'block';
+          });
+        } else if (tagValue == 'travel') {
+          selectionTravel.forEach((cardTravel) => {
+            cardTravel.style.display = 'block';
+          });
+        } else if (tagValue == 'sports') {
+          selectionSport.forEach((cardSport) => {
+            cardSport.style.display = 'block';
+          });
+        } else if (tagValue == 'animals') {
+          selectionAnimals.forEach((cardAnimals) => {
+            cardAnimals.style.display = 'block';
+          });
+        } else if (tagValue == 'events') {
+          selectionEvents.forEach((cardEvents) => {
+            cardEvents.style.display = 'block';
+          });
+        } else if (tagValue == 'all') {
+          cardSelections.forEach((cardSelection) => {
+            cardSelection.style.display = 'block';
+          });
+        }
+      });
+    });
   }
+
   showCards();
 }
 
